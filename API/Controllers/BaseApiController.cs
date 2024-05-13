8c8ae11d26bd9c6672b5176e566beb96be3ecbbe
daniyalashrafbranex <daniyal.ashraf@branex.com>
2024-05-13 20:11:33 +0500

    End of section-2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
